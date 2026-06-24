d36a07949d7987136655a920473aa876b2b4e755
Louise Bilous <bilouslouise@gmail.com>
2026-06-24 20:36:24 +0100

    Add Modules prefix.

--- SparkyRailwayIECC.View/SwindonRailwayPage.xaml.cs
+++ SparkyRailwayIECC.View/SwindonRailwayPage.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SparkyRailwayIECC.View
+namespace SparkyRailwayIECC.Modules.View
 {
     /// <summary>
     /// Interaction logic for SwindonRailway.xaml
